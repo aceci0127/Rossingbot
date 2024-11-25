67463106ef64b7833d8d8ec4134830a7ee17aa1d
aceci0127 <alfredo.ceci@studbocconi.it>
2024-11-25 17:46:41 +0100

    Commit

--- 1_chatbot.py
+++ 1_chatbot.py
@@ -11,28 +11,27 @@ st.title("Rossigbot   🐢")
 if 'conversation' not in st.session_state:
     st.session_state.conversation = []
 
-with st.container():
 # User input
-    if prompt := st.chat_input("Ask rossigbot anything..."):
-        # Display user message
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        # Append user message to conversation
-        st.session_state.conversation.append({"role": "user", "content": prompt})
+if prompt := st.chat_input("Ask rossigbot anything..."):
+    # Display user message
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Append user message to conversation
+    st.session_state.conversation.append({"role": "user", "content": prompt})
 
-        # Generate response
-        pipeline = AthenaPipeline(prompt, st.session_state.conversation, 'DORA')
-        response = pipeline.run_pipeline(prompt, st.session_state.conversation, 'DORA')
-        st.session_state.conversation.append(prompt)
-        st.session_state.conversation.append(response)
+    # Generate response
+    pipeline = AthenaPipeline(prompt, st.session_state.conversation, 'DORA')
+    response = pipeline.run_pipeline(prompt, st.session_state.conversation, 'DORA')
+    st.session_state.conversation.append(prompt)
+    st.session_state.conversation.append(response)
 
-        st.session_state.conversation.append({"role": "chatbot", "content": response})
+    st.session_state.conversation.append({"role": "chatbot", "content": response})
 
-        # Display assistant response
-        with st.chat_message("assistant"):
-            st.markdown(response)
-        # Append assistant response to conversation
-        st.session_state.conversation.append({"role": "assistant", "content": response})
+    # Display assistant response
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Append assistant response to conversation
+    st.session_state.conversation.append({"role": "assistant", "content": response})
 
 st.markdown("""
 <style>
@@ -41,7 +40,7 @@ st.markdown("""
         bottom: 0;
         right: 0;
         padding: 10px;
-        font-size: 16px;
+        font-size: 2px;
         background-color: rgba(255, 255, 255, 0.7);
     }
 </style>
