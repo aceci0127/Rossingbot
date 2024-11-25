63e4b57adf1ac0555a0911ce45f6c67e5f584cc6
aceci0127 <alfredo.ceci@studbocconi.it>
2024-11-25 17:50:56 +0100

    Commit

--- 1_chatbot.py
+++ 1_chatbot.py
@@ -14,10 +14,10 @@ if 'conversation' not in st.session_state:
 # User input
 if prompt := st.chat_input("Ask rossigbot anything..."):
     # Display user message
-    with st.chat_message("potatoe", ):
+    with st.chat_message("🥔", ):
         st.markdown(prompt)
     # Append user message to conversation
-    st.session_state.conversation.append({"role": "potato", "content": prompt})
+    st.session_state.conversation.append({"role": "🥔", "content": prompt})
 
     # Generate response
     pipeline = AthenaPipeline(prompt, st.session_state.conversation, 'DORA')
@@ -25,10 +25,10 @@ if prompt := st.chat_input("Ask rossigbot anything..."):
     st.session_state.conversation.append(prompt)
     st.session_state.conversation.append(response)
 
-    st.session_state.conversation.append({"role": "ai", "content": response})
+    st.session_state.conversation.append({"role": "assistant", "content": response})
 
     # Display assistant response
     with st.chat_message("assistant"):
         st.markdown(response)
     # Append assistant response to conversation
-    st.session_state.conversation.append({"role": "ai", "content": response})
+    st.session_state.conversation.append({"role": "assistant", "content": response})
