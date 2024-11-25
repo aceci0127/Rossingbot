142bf93de8860fb3b27cd50cd04f10d921667fa7
aceci0127 <alfredo.ceci@studbocconi.it>
2024-11-25 12:31:09 +0100

    Commit

--- 1_chatbot.py
+++ 1_chatbot.py
@@ -5,7 +5,7 @@ from pipeline import AthenaPipeline
 st.set_page_config(page_title="Chat with RossigBot ", page_icon="🐢")
 
 # Title for the Streamlit app
-st.title("FrenchBot🇫🇷")
+st.title("Rossigbot   🐢")
 
 # Initialize session state
 if 'conversation' not in st.session_state:
