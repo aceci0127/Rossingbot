0524bb4dd2acb20c4e943f45fbd24abd371612a4
aceci0127 <alfredo.ceci@studbocconi.it>
2024-11-25 17:53:49 +0100

    Commit

---
+++ pages/2_settings.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Set the page configuration
+st.set_page_config(page_title="Settings", page_icon="🐢")
+
+# Title for the Streamlit app
+st.title("Still no setting here patata, tra poco metto qualcosa")
